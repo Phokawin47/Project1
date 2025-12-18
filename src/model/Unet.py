954d53f07810e83b7c87d115de3e07e2a1f1f668
Phokawin47 <Phokawin47@gmail.com>
2025-12-18 15:31:12 +0700

    add multi rican Noise

--- src/model/Unet.py
+++ src/model/Unet.py
@@ -11,11 +11,12 @@ def main():
     image_size = (288, 288)  # กำหนดขนาดรูป (height, width)
     use_grayscale = True  # ถ้า True จะใช้ 1 ช่องสัญญาณ (อย่าลืมปรับโมเดลข้างล่าง)
     noise_factor = 0.2
+    noise_range=(0.2, 0.5)
     batch_size = 16
     val_ratio = 0.2
     num_workers= 4
     # ====== Dataset / Split ======
-    dataset = BrainTumorDataset(root_dir, image_size=image_size, transform=None, noise_factor=noise_factor, use_grayscale=use_grayscale)
+    dataset = BrainTumorDataset(root_dir, image_size=image_size, transform=None, noise_factor=noise_factor, noise_range=noise_range, return_noise_level=True, use_grayscale=use_grayscale)
     n_total = len(dataset)
     n_test = int(n_total * 0.1)  # 10% for test
     n_val = int(n_total * val_ratio)
@@ -27,7 +28,7 @@ def main():
     test_loader  = DataLoader(test_ds,  batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # ====== แสดง Shape ของข้อมูล ======
-    sample_noisy, sample_clean = next(iter(train_loader))
+    sample_noisy, sample_clean, noise = next(iter(train_loader))
     channels = 1 if use_grayscale else 3
     print(f"Dataset Info:")
     print(f"  - Total images: {n_total}")
@@ -35,6 +36,7 @@ def main():
     print(f"  - Val images: {n_val}")
     print(f"  - Test images: {n_test}")
     print(f"  - Image size: {image_size}")
+    print(f"  - Noise factor: {noise}")
     print(f"  - Channels: {channels} ({'Grayscale' if use_grayscale else 'RGB'})")
     print(f"  - Batch shape: {sample_noisy.shape} (batch_size, channels, height, width)")
     print(f"  - Device: {device}")
